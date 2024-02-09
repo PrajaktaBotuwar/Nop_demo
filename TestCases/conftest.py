bc50a924bf067568a8ddcbd5c7231e28399358b4
PrajaktaBotuwar <prachibotuwar@gmail.com>
2024-02-09 17:21:50 +0530

    Run bat

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -1,43 +1,43 @@
 import pytest
 from selenium import webdriver
-@pytest.fixture
-def setup():
-    driver = webdriver.Chrome()
-    driver.get("https://admin-demo.nopcommerce.com/")
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    return driver
-
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
-#
-#
-# # we need to add command liner --browser
-# def pytest_addoption(parser):
-#     parser.addoption("--browser")
-#
-#
 # @pytest.fixture
-# def setup(request):
-#     browser = request.config.getoption("--browser")
-#     if browser == "chrome":
-#         print("Test Run - Browser Chrome")
-#         driver = webdriver.Chrome()
-#         driver.get("https://admin-demo.nopcommerce.com/")
-#     elif browser == "firefox":
-#         print("Test Run - Browser Firefox")
-#         driver = webdriver.Firefox()
-#     elif browser == "edge":
-#         print("Test Run - Browser Edge")
-#         driver = webdriver.Edge()
-#     else:
-#         driver = webdriver.Chrome()
-#         print("Test Run - Browser Headless")
-#         driver = webdriver.Chrome(options=chrome_options)
-#     driver.maximize_window()
+# def setup():
+#     driver = webdriver.Chrome()
 #     driver.get("https://admin-demo.nopcommerce.com/")
-#     driver.implicitly_wait(5)
-#     yield driver
+#     driver.maximize_window()
+#     driver.implicitly_wait(10)
+#     return driver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+
+
+# we need to add command liner --browser
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture
+def setup(request):
+    browser = request.config.getoption("--browser")
+    if browser == "chrome":
+        print("Test Run - Browser Chrome")
+        driver = webdriver.Chrome()
+        driver.get("https://admin-demo.nopcommerce.com/")
+    elif browser == "firefox":
+        print("Test Run - Browser Firefox")
+        driver = webdriver.Firefox()
+    elif browser == "edge":
+        print("Test Run - Browser Edge")
+        driver = webdriver.Edge()
+    else:
+        driver = webdriver.Chrome()
+        print("Test Run - Browser Headless")
+        driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+    driver.get("https://admin-demo.nopcommerce.com/")
+    driver.implicitly_wait(5)
+    yield driver
 #     driver.quit()
 
 
